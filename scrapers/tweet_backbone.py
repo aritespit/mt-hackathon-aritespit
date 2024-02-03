c70ec264c988a390d1072f6c09d438df2b5afa37
cocolyy <arslanog20@itu.edu.tr>
2024-02-03 15:01:42 +0300

    tweet id added in dataset

--- scrapers/tweet_backbone.py
+++ scrapers/tweet_backbone.py
@@ -343,7 +343,7 @@ class Tweet:
 
 
             temp_driver.get(self.tweet_link)
-            sleep(4)
+            sleep(10)
             contents = temp_driver.find_elements("xpath",'(.//div[@data-testid="tweetText"])[1]/span | (.//div[@data-testid="tweetText"])[1]/a',
             )
             
--- scrapers/tweet_scrap.py
+++ scrapers/tweet_scrap.py
@@ -40,18 +40,19 @@ def read_and_save():
     Function that reads the scraped tweets and saves them to into a single CSV file.
     """
     folder_path = 'tweets/' if os.path.isdir('tweets/') else 'twitter_scraper/tweets/'
-    folder_name = os.listdir(folder_path)
-
     datasets = []
     for file in os.listdir(folder_path):
         if file.endswith('.csv'):
             df = pd.read_csv(folder_path + file)
             print(f'df handle: {df["Handle"].unique()} | folder name: {file.split(".")[0]}')
-            df = df[df['Handle'] == f'{file.split(".")[0]}']
+            df = df[df['Handle'].str.lower() == f'{file.split(".")[0].lower()}']
             datasets.append(df)
 
     combined_dataset = pd.concat(datasets)
-    columns_to_keep = ['index', 'Name', 'Handle', 'Timestamp', 'Content', 'Photo Link']
+    print(combined_dataset['Handle'].unique())
+    print(len(combined_dataset['Handle'].unique()))
+
+    columns_to_keep = ['index', 'Name', 'Handle', 'Timestamp', 'Content', 'Photo Link', 'Tweet ID']
     columns_to_drop = [col for col in combined_dataset.columns if col not in columns_to_keep]
     combined_dataset.drop(columns=columns_to_drop, inplace=True)
     combined_dataset.rename(columns={'Photo Link': 'photo_link'}, inplace=True)
